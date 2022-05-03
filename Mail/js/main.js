f0434f493c5b1de2ee7fef8d1bdce74dd4b17a4c
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-03 16:18:35 +0200

    bozza non funzionante

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -1,3 +1,23 @@
 // Chiedi all’utente la sua email,
 // controlla che sia nella lista di chi può accedere,
 // stampa un messaggio appropriato sull’esito del controllo.
+
+// creiamo una lista di email
+
+const iscritti = ('1email@dominio.it','2email@dominio.it','3email@dominio.it');
+
+// chiediamo email tramite prompt
+
+let mailUtente = prompt("Inserire la tua email");
+
+// accesso consentito / accesso negato
+
+if (mailUtente = iscritti){
+    console.log("Autorizzato");
+{
+else if (mailUtente != iscritti); {
+    console.log("Non Autorizzato")
+    let newEmail = prompt("Inserire nuova email");
+    iscritti.push("newEmail");
+}
+
