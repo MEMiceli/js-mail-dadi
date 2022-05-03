1fd19b612b25c2787e1d7946b6eea2d0549f6ee7
MEMiceli <marcello.emilio.miceli@gmail.com>
2022-05-03 17:14:09 +0200

    in evoluzione

--- Mail/js/main.js
+++ Mail/js/main.js
@@ -4,20 +4,29 @@
 
 // creiamo una lista di email
 
-const iscritti = ('1email@dominio.it','2email@dominio.it','3email@dominio.it');
+const iscritti = ['1email@dominio.it','2email@dominio.it','3email@dominio.it'];
 
 // chiediamo email tramite prompt
 
 let mailUtente = prompt("Inserire la tua email");
 
+// controllare l'email è presente
+
+for (let i = 0; i < iscritti.length; i++) {
+
 // accesso consentito / accesso negato
 
-if (mailUtente = iscritti){
-    console.log("Autorizzato");
+    if (mailUtente === iscritti[i]){
+        console.log("Autorizzato");
+    }
+    else if (mailUtente != i){
+        console.log("Non Autorizzato")
+        let newEmail = prompt("Inserire nuova email");
+        iscritti.push("newEmail");
+    }
+    
 }
-else (mailUtente != iscritti)
-    console.log("Non Autorizzato")
-    let newEmail = prompt("Inserire nuova email");
-    iscritti.push("newEmail");
+
+
 
 
